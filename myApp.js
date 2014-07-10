48eec884c2e7cfc773fd48aba600c5a5c6aa9ae3
Jordan Hudgens <jordan@wow.com>
2014-07-10 16:34:02 -0500

    integrated the ability to display the search query and clear out the search field after a search has taken place

--- myApp.js
+++ myApp.js
@@ -1,15 +1,11 @@
 var app = angular.module("InstaSearch", []);
 
-// Creamos y registramos el nuevo servicio de instagram
 app.factory('instagram', ['$http', function($http){
 
 	return {
 		fetchPopular: function(callback, myQuery){
             
       var endPoint = "https://api.instagram.com/v1/tags/" + myQuery + "/media/recent?client_id=4efb143562a54f5f947ec21329a0890f&callback=JSON_CALLBACK";
-
-      // Try using this:
-      // http://angular-ui.github.io/bootstrap/#/typeahead
       
       $http.jsonp(endPoint).success(function(response){
           callback(response.data);
@@ -33,17 +29,18 @@ function ($scope, instagram){
     };
 
 	$scope.pics = [];
+  var isTextOpen = false;
+  $scope.resultValue = "";
 
   $scope.fetchPics = function (tag) {
-        instagram.fetchPopular(function(data){
+
+    $scope.isTextOpen = true;  
+    
+    instagram.fetchPopular(function(data){
       $scope.pics = data;
-      $scope.fetchPics(newValue);
+      $scope.resultValue = tag;
+      $scope.searchQuery = "";
     }, tag);
   }
-
-  // $scope.$watch('searchQuery', function(newValue, oldValue) {
-  //   $scope.fetchPics(newValue);
-  // });
-
 
 }]);
